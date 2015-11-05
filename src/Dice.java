7a1ab7092201c59ed3efea5f004d13d8f8bca0f7
a213 <a213@ISA2130029.sac.rsccd.inst>
2015-11-04 20:01:43 -0800

    LAb 4-2

--- src/Dice.java
+++ src/Dice.java
@@ -14,7 +14,6 @@ public class Dice{
 		number =num;
 		}
 
-		
 		public int OneDice()
 		{
 			
@@ -29,8 +28,6 @@ public class Dice{
 			return random;
 		}
 			
-			
-		
 		public int ThreeDice(){
 			int random = (int) ((Math.random()* 18)+2);
 			valueNumber = random;
@@ -39,7 +36,6 @@ public class Dice{
 		}
 		
 	//lab week11	
-		
 			public Dice(){
 			
 		}
@@ -49,23 +45,15 @@ public class Dice{
 			valueNumber = random;
 			return random;
 		}
-		
-	
-		
+
 		public int Value(){
 			return valueNumber;
 		}
 		
-		public int Throw(int bounce){
-			int i=0;
-			while(i<bounce)
-					{Throw();
-					i++;}
-			
-			return valueNumber;
+		
 			
 			
-		}
+		
 }
 		
 	
